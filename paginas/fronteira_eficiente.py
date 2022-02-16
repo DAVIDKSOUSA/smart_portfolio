03e73910d3bb489122573a20d3c5a042633c921f
David Sousa <DAVID_AFA84@HOTMAIL.COM>
2022-02-16 19:47:03 -0300

    modificações diversas

--- paginas/fronteira_eficiente.py
+++ paginas/fronteira_eficiente.py
@@ -108,6 +108,7 @@ def fronteira_eficiente():
     #fig.update_yaxes(range=[0.1,0.1])
     fig.update_layout(coloraxis_colorbar=dict(title="Sharpe Ratio"))
     st.plotly_chart(fig)
+    st.write('___')
 
 
 
