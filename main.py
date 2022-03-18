52027de240c8197462f7970525adb2dc7151f3c1
David Sousa <DAVID_AFA84@HOTMAIL.COM>
2022-03-17 23:40:21 -0300

    modificações diversas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #baixar as bibliotecas
 import streamlit as st
 from streamlit_option_menu import option_menu
+
 #importar paginas
 import paginas.fronteira_eficiente as fe
 import paginas.relatorio_performance as rp
